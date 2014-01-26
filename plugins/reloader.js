3e4ad891e535e5b2efb9c474f733d524e8516a67
Johan Liljegren <datanizze@gmail.com>
2014-01-26 23:11:28 +0100

    reloader plugin, POC for using the passed irc handler from new plugin layout

---
+++ plugins/reloader.js
@@ -0,0 +1,16 @@
+module.exports = function(ircHandler) {
+  'use strict';
+
+  var self = this;
+  self.ircHandler = ircHandler;
+  console.log('RELOADER: Plugin loaded');
+
+  return {
+    'message': {
+      'match': [/reload/i],
+      'handler': function(from, to, message) {
+        self.ircHandler.reloadPlugin('example.js');
+      }
+    }
+  };
+};
