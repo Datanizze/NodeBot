5756dce46e8644f47540baab82574ded3ceba58d
Johan Liljegren <datanizze@gmail.com>
2014-02-16 15:46:49 +0100

    Rewritten about three times, current iteration is not heavily inspired by connect (but far from as sophisticated)

--- app.js
+++ app.js
@@ -1,40 +1,22 @@
-var irc = require('irc');
-var fs = require('fs');
-var _ = require('underscore');
-var Handler = require('./IrcHandler');
+var NodeBot = require('./lib/nodebot');
 
-
-
-
-
-// initiate the irc client
-var client = new irc.Client('irc.bsnet.se', 'notMarvin', {
+var bot = new NodeBot('irc.bsnet.se', 'notMarvin', {
   channels: ['#johansbot'],
-  userName: 'Johans bot',
-  realName: 'Bot Botsson'
-});
-
-var handler = new Handler.IrcHandler(client, 'perms');
-
-
-
-
-client.once('registered', function(message) {
-  console.log('Registered with server, loading plugins');
-
-  fs.readdir('./plugins', function(err, files) {
-    if (err) {
-      throw err;
-    }
-
-    files.forEach(function (file) {
-      handler.loadPlugin(file, './plugins/' + file);
-    });
-
-  });
-
+  userName: 'Johansbot',
+  realName: 'Bot Botsson',
+  autoRejoin: false
 });
 
 
+bot.use(function first(req, res, next) {
+  req.stamp = 'I WAS HERE';
+  // console.log('next', next);
+  next();
+});
 
 
+bot.on('message', new RegExp('^!'), function(req, res) {
+  console.log('REQ:', req);
+  console.log('RES:', res);
+  bot.say(req.to, req.from + ' SAID: ' + req.text);
+});
