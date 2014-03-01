2a46494057134bda3baeda9aecc689100c92ac28
Johan Liljegren <datanizze@gmail.com>
2014-03-01 17:33:42 +0100

    Replaced hardcoded config with values from the config file

--- handlers/coc-handler.js
+++ handlers/coc-handler.js
@@ -1,24 +1,16 @@
 var request = require('request');
 var mongo = require('mongoskin');
-
+var config = require('../config').cocs;
 
 function CocHandler(cacheTime) {
   'use strict';
   cacheTime = cacheTime * 1000; // seconds to  milliseconds
   // TODO: make these config variables instead.. hard coding sucks
-  var baseUrl = 'http://dbwebb.se/coc';
+  var baseUrl = config.baseUrl;
 
-  var mongo_config = {
-    host: 'localhost',
-    port: '', // 27017 is default
-    username: '',
-    password: '',
-    database: 'wip-cocs'
-  };
+  var db = mongo.db(getSimpleMongoConnectionString(config.mongo));
 
-  var db = mongo.db(getSimpleMongoConnectionString(mongo_config));
-
-  var cocs = db.collection('real-cocs');
+  var cocs = db.collection(config.mongo.collection);
   cocs.ensureIndex({'id': 1}, { unique: true, sparse: true}, function(error, result) {
     // TODO: check for errors?
     // We shouldn't have any race conditions here,
